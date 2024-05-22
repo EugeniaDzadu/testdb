3fa50eec070da0f06b78fcc5f337e0187c2edd60
EugeniaDzadu <jeeniaace@gmail.com>
2024-05-22 13:52:46 +0000

    error to fix

--- form.js
+++ form.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require("mongoose")
+express.urlencoded({extended:true})
 
 // Middleware to parse JSON data
 app.use(express.json());
@@ -11,6 +12,30 @@ mongoose.connect(connectionUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+// HTML Route
+app.get("/", (req, res)=>{
+    res.render("index")
+})
+
+//  Import the userModel from the models folderImport
+//  the userModel from the models folder
+const UserModel = require("./models/user")
+
+
+// route for collecting and sending data to the DB
+app.post("/api/user", (req, res)=>{
+})
+
+// Pass req.body as an argument in the route
+const SaveUser = new UserModel(req.body)
+
+// userinput save
+
+
+// Set the view engine to ejs
+app.set("view engine", "ejs")
+
 
 app.get("/", (req, res)=>{
     res.send("Hello")
